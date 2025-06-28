3af627233a41a763d84237fc3ece49415bb03242
Basic-Nature <misterjcbarragan@gmail.com>
2025-06-27 18:51:16 -0700

    modified:   webapp/parser/bots/librarian.py

--- webapp/parser/bots/librarian.py
+++ webapp/parser/bots/librarian.py
@@ -5,7 +5,6 @@ import orjson
 import subprocess
 import sys
 import time
-from ..bots.librarian import save_context_library
 from tempfile import NamedTemporaryFile
 import shutil
 from pathlib import Path
